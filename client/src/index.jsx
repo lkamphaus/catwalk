dc8b5121551c44c255fe5c784d4e9d8591478d94
Jacob Melnick <jake.r.emelnick@gmail.com>
2021-06-02 17:12:07 -0700

    React Rendering

---
+++ client/src/index.jsx
@@ -0,0 +1,22 @@
+import React, {useState} from 'react'
+import ReactDOM from 'react-dom';
+
+const App = () => {
+
+// const [text, setText] = useState('')
+
+
+
+// setText('hello World')
+
+
+return (
+    <div>Hello World</div>
+)
+
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
+
+export default App
+
