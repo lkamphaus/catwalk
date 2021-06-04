d762487f5e4b41b98ae200e456a71025f2384fff
Laura Kamphaus <lmkamphaus@gmail.com>
2021-06-04 15:11:57 -0700

    Created route to get product id

---
+++ client/src/components/ProductDetail.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import Config from '../../.././config.js';
+
+const ProductDetail = () => {
+  const [prod, setProd] = useState(null);
+  const { product_id } = useParams();
+
+  useEffect(() => {
+    fetch(`http://localhost:3246/products/${product_id}`, {
+      headers: {
+        "Content-Type": "application/json",
+      }
+    })
+      .then(setProd)
+      .catch(err => console.log('err', err))
+  }, product_id)
+
+  return (
+    <div>
+      <div>{prod}</div>
+    </div>
+  )
+}
+
+export default ProductDetail;
